b186b302f6e24d9e232ac84e5b09f4d8a2859ef6
NickFleece <nebf97@gmail.com>
2018-11-05 00:00:28 -0500

    Port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
@@ -8,4 +8,4 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
