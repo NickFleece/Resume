18ff04758092292858c7c63ec08b1de244d76e09
Nick Fleece <nebf97@gmail.com>
2018-11-05 23:03:25 -0500

    Added css, more visual setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
