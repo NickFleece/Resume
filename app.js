015f01e52a412f4733383c25f866e14be63dc527
NickFleece <nebf97@gmail.com>
2022-03-26 23:54:01 -0400

    Lots of implementations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
